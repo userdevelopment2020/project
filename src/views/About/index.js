05fecbacda1dae03d4fd1b1badf4365617700ce6
Lochan <lochan@linkites.com>
2020-05-13 23:24:18 +0530

    Setup

---
+++ src/views/About/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.scss";
+
+class About extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<>
+				<h1>About</h1>
+			</>
+		);
+	}
+}
+export default About
---
+++ src/views/Home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.scss";
+
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<>
+				<h1>HOME</h1>
+			</>
+		);
+	}
+}
+export default Home
